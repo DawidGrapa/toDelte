0e4c64688bb95bdc90bf2510524b7b3fd32d7a47
dgrapa <dgrapa@guidewire.com>
2022-07-31 22:45:46 +0200

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { NONE_TYPE } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { PartService } from './service/part-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private partService: PartService) {}
+  ngOnInit() {}
+
+  partName: string = '';
+  searching: boolean = false;
   title = 'partsf';
+  response: any = [];
+
+  find() {
+    this.searching = true;
+    this.partService.getAllParts(this.partName).subscribe(
+      (data) => {
+        this.response = data;
+        this.searching = false;
+        console.log(this.response);
+      }
+    );
+  }
 }
